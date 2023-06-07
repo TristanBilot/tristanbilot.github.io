14eacd9126bda313f537a6e8433a2e7d1ee223cc
Tristan Bilot <bilot.tristan@hotmail.fr>
2023-06-07 11:39:37 +0200

    add 2 survey papers in publications

--- src/Components/Publications.js
+++ src/Components/Publications.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import kernel from '../resources/publications/phishgnn_figure.png';
+import intrusion_survey from '../resources/publications/intrusion_survey_figure.png';
+import malware_survey from '../resources/publications/malware_survey_figure.png';
 import '../Styles/publications.sass';
 
 class Publications extends Component {
@@ -20,20 +22,64 @@ class Publications extends Component {
                     <div className="small-vertical-space"></div>
                     <div className="row animatable fadeInUp">
                         <div className="col-md-12">
-                            <div class="publication-card animatable fadeInUp accent-box">
-                                <div class="">
-                                    <h6>SECRYPT 2022 - Lisbon</h6>
-                                    <h3>PhishGNN: A Phishing Website Detection Framework using Graph Neural Networks</h3>
-                                    <span className=""><u>Tristan Bilot</u>, Grégoire Geis, Badis Hammi</span>
-                                </div>
-                                <div class="d-flex align-items-center justify-content-center">
-                                    <img className="publication-card__image" src={kernel} alt="PhishGNN"></img>
-                                </div>
-                                <div>
-                                    <a href='https://www.researchgate.net/publication/361002272_PhishGNN_A_Phishing_Website_Detection_Framework_using_Graph_Neural_Networks' target="_blank" rel="noopener">Paper</a> 
-                                    <a href='https://github.com/TristanBilot/phishGNN' target="_blank" rel="noopener">Code</a>
-                                </div>
-                            </div>
+
+                        <div class="publication-card animatable fadeInUp accent-box">
+                              <div class="">
+                                  <h6>IEEE Access Journal</h6>
+                                  <h3>
+                                    <a href='https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=10123384' target="_blank" rel="noopener">
+                                      Graph Neural Networks for Intrusion Detection: A Survey
+                                    </a>
+                                  </h3>
+                                  <span className=""><u>Tristan Bilot</u>, Nour El Madhoun, Khaldoun Al Agha, Anis Zouaoui</span>
+                              </div>
+                              <div class="d-flex align-items-center justify-content-center">
+                                  <img className="publication-card__image" src={intrusion_survey} alt="Intrusion survey GNNs"></img>
+                              </div>
+                              <div>
+                                  <a href='https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=10123384' target="_blank" rel="noopener">Paper</a> 
+                              </div>
+                          </div>
+                          <div className="small-vertical-space"></div>
+
+                          <div class="publication-card animatable fadeInUp accent-box">
+                              <div class="">
+                                  <h6>Submitted to ACM Computing Surveys (preprint)</h6>
+                                  <h3>
+                                    <a href='https://arxiv.org/abs/2303.16004' target="_blank" rel="noopener">
+                                      A Survey on Malware Detection with Graph Representation Learning
+                                    </a>
+                                  </h3>
+                                  <span className=""><u>Tristan Bilot</u>, Nour El Madhoun, Khaldoun Al Agha, Anis Zouaoui</span>
+                              </div>
+                              <div class="d-flex align-items-center justify-content-center">
+                                  <img className="publication-card__image" src={malware_survey} alt="Malware survey GNNs"></img>
+                              </div>
+                              <div>
+                                  <a href='https://arxiv.org/abs/2303.16004' target="_blank" rel="noopener">Paper</a> 
+                              </div>
+                          </div>
+                          <div className="small-vertical-space"></div>
+
+                          <div class="publication-card animatable fadeInUp accent-box">
+                              <div class="">
+                                  <h6>SECRYPT 2022 - Lisbon</h6>
+                                  <h3>
+                                    <a href='https://www.researchgate.net/publication/361002272_PhishGNN_A_Phishing_Website_Detection_Framework_using_Graph_Neural_Networks' target="_blank" rel="noopener">
+                                      PhishGNN: A Phishing Website Detection Framework using Graph Neural Networks
+                                    </a>
+                                  </h3>
+                                  <span className=""><u>Tristan Bilot</u>, Grégoire Geis, Badis Hammi</span>
+                              </div>
+                              <div class="d-flex align-items-center justify-content-center">
+                                  <img className="publication-card__image" src={kernel} alt="PhishGNN"></img>
+                              </div>
+                              <div>
+                                  <a href='https://www.researchgate.net/publication/361002272_PhishGNN_A_Phishing_Website_Detection_Framework_using_Graph_Neural_Networks' target="_blank" rel="noopener">Paper</a> 
+                                  <a href='https://github.com/TristanBilot/phishGNN' target="_blank" rel="noopener">Code</a>
+                              </div>
+                          </div>
+
                         </div>
 
                     </div>
