d0467fc3df45769d11064dc7c5c256031758ab50
Tristan Bilot <bilot.tristan@hotmail.fr>
2024-05-25 14:59:29 -0700

    update bio + add X link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,10 +7,11 @@ class Header extends Component {
 
   render() {
     return (
-      <div id="intro" className="header full-height">
+      <div id="intro" className="header">
+      <div class="header-top-space"></div>
 
         <div className="container-fluid position-relative header-global">
-          <div className="row align-items-center full-height">
+          <div className="row align-items-center">
             <div className="col-lg-2 col-1 col-lg-"></div>
             <div className="col-lg-8 col-10">
 
@@ -23,7 +24,7 @@ class Header extends Component {
 
                     <div class="portrait-title">
                       <h2 itemProp="name">Tristan Bilot</h2>
-                      <h3 itemProp="jobTitle">PhD student &amp; Research Scientist</h3>
+                      <h3 itemProp="jobTitle">Ph.D. student &amp; Research Scientist</h3>
                     </div>
 
                     <ul class="network-icon" aria-hidden="true">
@@ -53,8 +54,8 @@ class Header extends Component {
                         </a>
                       </li>
                       <li>
-                        <a itemProp="sameAs" href="https://twitter.com/tristanbilot" target="_blank" rel="noopener">
-                          <i class="fa-brands fa-x-twitter"></i>
+                        <a itemProp="sameAs" href="https://x.com/tristanbilot" target="_blank" rel="noopener">
+                          <i class="fa-brands fa-x-twitter big-icon"></i>
                         </a>
                       </li>
                       {/* <li>
@@ -71,14 +72,16 @@ class Header extends Component {
 
 
 
-                  <h1 id="about-me">About me 👋</h1>
+                  <h1 id="about-me">About me</h1>
                   <p></p>
 
-                  <p>I am a PhD student from <a className='word-accent' href="https://www.universite-paris-saclay.fr/en/about/universite-paris-saclay"> Université Paris-Saclay</a>, member of <a className='word-accent' href="https://www.lisn.upsaclay.fr/">LISN</a> & <a className='word-accent' href="https://www.isep.fr/laboratoire-lisite-2-2/">LISITE</a> laboratories and Research Scientist at <a className='word-accent' href="https://www.iriguard.com/"> Iriguard</a>.</p>
+                  <p>I am a Ph.D. student from <a className='word-accent' href="https://www.universite-paris-saclay.fr/en/about/universite-paris-saclay"> Université Paris-Saclay</a>, member of <a className='word-accent' href="https://www.lisn.upsaclay.fr/?lang=en">LISN</a> & <a className='word-accent' href="https://en.isep.fr/research_at_isep/">LISITE</a> laboratories and Research Scientist at <a className='word-accent' href="https://www.iriguard.com/"> Iriguard</a>.
+                  Currently, I am a visiting research student in the CS department at the <a className='word-accent' href="https://www.cs.ubc.ca/">University of British Columbia</a> (<a className='word-accent' href="https://systopia.cs.ubc.ca/">Systopia Lab</a>), under the supervision of <a className='word-accent' href="https://tfjmp.org/">Thomas Pasquier</a>.</p>
 
-                  <p>My primary area of research involves applying Graph Deep Learning techniques to Cybersecurity.</p>
+                  <p>My research focuses on leveraging Graph Deep Learning for the detection of complex attacks and anomalies. I work on topics such as Graph Neural Networks (GNNs), temporal graphs, Self-Supervised Learning (SSL), and intrusion detection at the network- and system-levels.</p>
 
-                  <p>In life, I'm interested in Cosmology, life origins and traveling!</p>
+                  <p>In my free time, I actively contribute to open-source projects. Most notably, I have contributed to <a className='word-accent' href="https://github.com/ml-explore/mlx">Apple's MLX</a> framework, by implementing the backpropagation of multiple scattering operations. Alongside <a className='word-accent' href="https://scholar.google.it/citations?user=_7glimkAAAAJ&hl=it">Francesco Farina</a>, I co-developed <a className='word-accent' href="https://github.com/mlx-graphs/mlx-graphs">MLX-graphs</a>, a GPU-accelerated GNN library for Apple Silicon.
+                  In life, I am passionate about Cosmology, the origins of life, and traveling!</p>
 
 
                   <div class="row">
@@ -88,6 +91,18 @@ class Header extends Component {
                       <h3>Recent Experience</h3>
                       <ul class="ul-edu fa-ul">
 
+                      <li>
+                          <i class="fa-li fa fa-briefcase"></i>
+                          <div class="description">
+                            <p class="course">Visiting Research Student</p>
+
+                            <a href="https://www.cs.ubc.ca/">
+                              <p class="institution">University of British Columbia (UBC)</p>
+                            </a>
+
+                          </div>
+                        </li>
+                        
                         <li>
                           <i class="fa-li fa fa-briefcase"></i>
                           <div class="description">
@@ -103,7 +118,7 @@ class Header extends Component {
                         <li>
                           <i class="fa-li fa fa-briefcase"></i>
                           <div class="description">
-                            <p class="course">Data Engineer apprentice</p>
+                            <p class="course">Software/Data Engineer apprentice, 2019-2022</p>
 
                             <a href="https://www.carrefour.fr/">
                               <p class="institution">Carrefour-Google AI Lab</p>
@@ -124,7 +139,7 @@ class Header extends Component {
                         <li>
                           <i class="fa-li fa fa-graduation-cap"></i>
                           <div class="description">
-                            <p class="course">PhD student</p>
+                            <p class="course">Ph.D. student, 2022-</p>
 
                             <a href="https://www.universite-paris-saclay.fr/en/about/universite-paris-saclay">
                               <p class="institution">Université Paris-Saclay</p>
@@ -136,10 +151,22 @@ class Header extends Component {
                         <li>
                           <i class="fa-li fa fa-graduation-cap"></i>
                           <div class="description">
-                            <p class="course">Engineer in Computer Science (MSc)</p>
+                            <p class="course">Diplôme d'Ingénieur (MSc), 2019-2022</p>
 
                             <a href="http://www.epita.fr">
                               <p class="institution">EPITA</p>
+                            </a>
+
+                          </div>
+                        </li>
+
+                        <li>
+                          <i class="fa-li fa fa-graduation-cap"></i>
+                          <div class="description">
+                            <p class="course">Diplôme Universitaire de Technologie, 2017-2019</p>
+
+                            <a href="https://u-paris.fr/en/">
+                              <p class="institution">Université Paris-Descartes</p>
                             </a>
 
                           </div>
--- src/Components/Publications.js
+++ src/Components/Publications.js
@@ -26,7 +26,26 @@ class Publications extends Component {
 
                         <div class="publication-card animatable fadeInUp accent-box">
                               <div class="">
-                                  <h6>CSNet 2023 - Montréal</h6>
+                                  <h6>ACM Computing Surveys (2024)</h6>
+                                  <h3>
+                                    <a href='https://dl.acm.org/doi/10.1145/3664649' target="_blank" rel="noopener">
+                                      A Survey on Malware Detection with Graph Representation Learning
+                                    </a>
+                                  </h3>
+                                  <span className=""><u>Tristan Bilot</u>, Nour El Madhoun, Khaldoun Al Agha, Anis Zouaoui</span>
+                              </div>
+                              <div class="d-flex align-items-center justify-content-center">
+                                  <img className="publication-card__image" src={malware_survey} alt="Malware survey GNNs"></img>
+                              </div>
+                              <div>
+                                  <a href='https://dl.acm.org/doi/10.1145/3664649' target="_blank" rel="noopener">Paper</a> 
+                              </div>
+                          </div>
+                          <div className="small-vertical-space"></div>
+
+                        <div class="publication-card animatable fadeInUp accent-box">
+                              <div class="">
+                                  <h6>CSNet (2023)</h6>
                                   <h3>
                                     <a href='https://hal.science/hal-04186579/document' target="_blank" rel="noopener">
                                     A Benchmark of Graph Augmentations for Contrastive Learning-Based Network Attack Detection with Graph Neural Networks
@@ -45,7 +64,7 @@ class Publications extends Component {
                         
                         <div class="publication-card animatable fadeInUp accent-box">
                               <div class="">
-                                  <h6>IEEE Access Journal</h6>
+                                  <h6>IEEE Access (2023)</h6>
                                   <h3>
                                     <a href='https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=10123384' target="_blank" rel="noopener">
                                       Graph Neural Networks for Intrusion Detection: A Survey
@@ -64,26 +83,7 @@ class Publications extends Component {
 
                           <div class="publication-card animatable fadeInUp accent-box">
                               <div class="">
-                                  <h6>Submitted to ACM Computing Surveys (preprint)</h6>
-                                  <h3>
-                                    <a href='https://arxiv.org/abs/2303.16004' target="_blank" rel="noopener">
-                                      A Survey on Malware Detection with Graph Representation Learning
-                                    </a>
-                                  </h3>
-                                  <span className=""><u>Tristan Bilot</u>, Nour El Madhoun, Khaldoun Al Agha, Anis Zouaoui</span>
-                              </div>
-                              <div class="d-flex align-items-center justify-content-center">
-                                  <img className="publication-card__image" src={malware_survey} alt="Malware survey GNNs"></img>
-                              </div>
-                              <div>
-                                  <a href='https://arxiv.org/abs/2303.16004' target="_blank" rel="noopener">Paper</a> 
-                              </div>
-                          </div>
-                          <div className="small-vertical-space"></div>
-
-                          <div class="publication-card animatable fadeInUp accent-box">
-                              <div class="">
-                                  <h6>SECRYPT 2022 - Lisbon</h6>
+                                  <h6>SECRYPT (2022)</h6>
                                   <h3>
                                     <a href='https://www.researchgate.net/publication/361002272_PhishGNN_A_Phishing_Website_Detection_Framework_using_Graph_Neural_Networks' target="_blank" rel="noopener">
                                       PhishGNN: A Phishing Website Detection Framework using Graph Neural Networks
